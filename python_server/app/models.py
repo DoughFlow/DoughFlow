ee291090730aaa1086aef4e225a7f31ed6f3c31b
EthanRohman93 <ethanmrohman@gmail.com>
2024-02-17 20:03:10 -0600

    app created data model started

---
+++ python_server/app/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class StockMarketData(models.Model):
+    timestamp = models.DateTimeField()
+    open = models.DecimalField(max_digits=10, decimal_places=2)
+    high = models.DecimalField(max_digits=10, decimal_places=2)
+    low = models.DecimalField(max_digits=10, decimal_places=2)
+    close = models.DecimalField(max_digits=10, decimal_places=2)
+    volume = models.IntegerField()
+
+    class Meta:
+        db_table = 'apple_stock_data'
