3d18078e352ae990b1ed362222de153cd3590791
EthanRohman93 <ethanmrohman@gmail.com>
2024-02-18 11:35:02 -0600

    data models are in and hypertable creation done

--- python_server/app/management/commands/create_hypertable.py
+++ python_server/app/management/commands/create_hypertable.py
@@ -1,1 +1,12 @@
+from django.core.management.base import BaseCommand
+from django.db import connection
+
+
+class Command(BaseCommand):
+    help = 'Converts stock_data table to a TimescaleDB hypertable'
+
+    def handle(self, *args, **options):
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT create_hypertable('stock_data', by_range('timestamp'));")
+            self.stdout.write(self.style.SUCCESS('Successfully created hypertable.'))
 
