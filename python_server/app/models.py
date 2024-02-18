f0dedca14d462c2e532e82a190e52b9bc5f5da47
EthanRohman93 <ethanmrohman@gmail.com>
2024-02-18 11:35:02 -0600

    data models are in and hypertable creation done

--- python_server/app/models.py
+++ python_server/app/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
+
 class StockMarketData(models.Model):
-    timestamp = models.DateTimeField()
+    timestamp = models.DateTimeField(db_index=True, primary_key=True)
+    ticker = models.CharField(max_length=25)
     open = models.DecimalField(max_digits=10, decimal_places=2)
     high = models.DecimalField(max_digits=10, decimal_places=2)
     low = models.DecimalField(max_digits=10, decimal_places=2)
