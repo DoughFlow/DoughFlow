b1c652b0186629eaa790c3255f18b5945fedfa62
Jack M <cubadox@gmail.com>
2024-04-04 12:07:45 -0500

    work in progress still.

--- frontend/app/components/candlestick_ts.tsx
+++ frontend/app/components/candlestick_ts.tsx
@@ -28,92 +28,92 @@ const CandleStickChartObs = ({data}: {data: StockData[]}) => {
     const svgRef = useRef(null);
 
     useEffect(() => {
-    d3.select(svgRef.current).selectAll('svg').remove();
-    
-    // declare positional encodings
-//    const x = d3.scaleBand()
-//        .domain(d3.utcDay
-//                .range(data.at(0)?.datetime, +data.at(-1).datetime + 1)
-//                .filter(d => d.getUTCDay() !== 0 && d.getUTCDay() !== 6))
-//        .range([margin.left, width-margin.right]).padding(0.2);
+            d3.select(svgRef.current).selectAll('svg').remove();
+
+            // declare positional encodings
+            //    const x = d3.scaleBand()
+            //        .domain(d3.utcDay
+            //                .range(data.at(0)?.datetime, +data.at(-1).datetime + 1)
+            //                .filter(d => d.getUTCDay() !== 0 && d.getUTCDay() !== 6))
+            //        .range([margin.left, width-margin.right]).padding(0.2);
 
 
-    const x = d3.scaleBand()
-    .domain(
-        d3.utcDay
-            .range(data.at(0)?.datetime, +data.at(-1).datetime + 1)
-            .filter(d => d.getUTCDay() !== 0 && d.getUTCDay() !== 6)
-            .map(d => d.toISOString()) // Convert Date objects to string representations
-    )
-    .range([margin.left, width-margin.right]).padding(0.2);
+            const x = d3.scaleBand()
+            .domain(
+                    d3.utcDay
+                    .range(data.at(0)?.datetime, +data.at(-1).datetime + 1)
+                    .filter(d => d.getUTCDay() !== 0 && d.getUTCDay() !== 6)
+                    .map(d => d.toISOString()) // Convert Date objects to string representations
+                   )
+            .range([margin.left, width-margin.right]).padding(0.2);
 
 
-    const y = d3.scaleLog()
-        .domain([d3.min(data, d => d.low), d3.max(data, d => d.high)])
-        .rangeRound([height - margin.bottom, margin.top]);
+            const y = d3.scaleLog()
+                .domain([d3.min(data, d => d.low), d3.max(data, d => d.high)])
+                .rangeRound([height - margin.bottom, margin.top]);
 
-    // create SVG Containter
-    // const svg = d3.create("svg")
-    const svg = d3.select(svgRef.current)
-        .append('svg')
-        .attr("viewBox", [0, 0, width, height]);
+            // create SVG Containter
+            // const svg = d3.create("svg")
+            const svg = d3.select(svgRef.current)
+                .append('svg')
+                .attr("viewBox", [0, 0, width, height]);
 
-    // append the axes
-    svg.append("g")
-        .attr("transform",`translate(0,${height - margin.bottom})`)
-        .call(d3.axisBottom(x)
-                .tickValues(d3.utcMonday
-                    .every(width > 720 ? 1:2)
-                    .range(data.at(0).datetime, data.at(-1).datetime))
-                .tickFormat(d3.utcFormat("%-m/%-d")))
-        .call(g => g.select(".domain").remove());
+            // append the axes
+            svg.append("g")
+                .attr("transform",`translate(0,${height - margin.bottom})`)
+                .call(d3.axisBottom(x)
+                        .tickValues(d3.utcMonday
+                            .every(width > 720 ? 1:2)
+                            .range(data.at(0).datetime, data.at(-1).datetime))
+                        .tickFormat(d3.utcFormat("%-m/%-d")))
+                .call(g => g.select(".domain").remove());
 
-    svg.append("g")
-        .attr("transform", `translate(${margin.left},0)`)
-        .call(d3.axisLeft(y)
-                .tickFormat(d3.format("$~f"))
-                .tickValues(d3.scaleLinear().domain(y.domain()).ticks()))
-        .call(g => g.selectAll(".tick line").clone()
-                .attr("stroke-opacity", 0.2)
-                .attr("x2", width - margin.left - margin.right))
-        .call(g => g.select(".domain").remove());
+            svg.append("g")
+                .attr("transform", `translate(${margin.left},0)`)
+                .call(d3.axisLeft(y)
+                        .tickFormat(d3.format("$~f"))
+                        .tickValues(d3.scaleLinear().domain(y.domain()).ticks()))
+                .call(g => g.selectAll(".tick line").clone()
+                        .attr("stroke-opacity", 0.2)
+                        .attr("x2", width - margin.left - margin.right))
+                .call(g => g.select(".domain").remove());
 
-    // create a group for each day of data, and append two lines to it
-    const g = svg.append("g")
-        .attr("stroke-linecap", "round")
-        .attr("stroke", "black")
-        .selectAll("g")
-        .data(data)
-        .join("g")
-        .attr("transform", d => `translate(${x(d.datetime.getUTCDay().toString())},0)`);
+            // create a group for each day of data, and append two lines to it
+            const g = svg.append("g")
+                .attr("stroke-linecap", "round")
+                .attr("stroke", "black")
+                .selectAll("g")
+                .data(data)
+                .join("g")
+                .attr("transform", d => `translate(${x(d.datetime.getUTCDay().toString())},0)`);
 
-    g.append("line")
-        .attr("y1", d => y(d.low))
-        .attr("y2", d => y(d.high));
+            g.append("line")
+                .attr("y1", d => y(d.low))
+                .attr("y2", d => y(d.high));
 
-    g.append("line")
-        .attr("y1", d => y(d.open))
-        .attr("y2", d => y(d.close))
-        .attr("stroke-width", x.bandwidth())
-        .attr("stroke", d => d.open > d.close ? d3.schemeSet1[0]
-                : d.close > d.open ? d3.schemeSet1[2]
-                : d3.schemeSet1[8]);
+            g.append("line")
+                .attr("y1", d => y(d.open))
+                .attr("y2", d => y(d.close))
+                .attr("stroke-width", x.bandwidth())
+                .attr("stroke", d => d.open > d.close ? d3.schemeSet1[0]
+                        : d.close > d.open ? d3.schemeSet1[2]
+                        : d3.schemeSet1[8]);
 
-    // append a title (tooltip)
-    const formatDate = d3.utcFormat("%B %-d, %Y");
-    const formatValue = d3.format(".2f");
-    const formatChange = ((f) => (y0: number, y1: number) => f((y1 - y0) / y0))(d3.format("+.2%"));
+            // append a title (tooltip)
+            const formatDate = d3.utcFormat("%B %-d, %Y");
+            const formatValue = d3.format(".2f");
+            const formatChange = ((f) => (y0: number, y1: number) => f((y1 - y0) / y0))(d3.format("+.2%"));
 
-    g.append("title")
-        .text(d => `${formatDate(d.datetime)}
+            g.append("title")
+                .text(d => `${formatDate(d.datetime)}
 Open: ${formatValue(d.open)}
 Close: ${formatValue(d.close)} (${formatChange(d.open, d.close)})
 Low: ${formatValue(d.low)}
 High: ${formatValue(d.high)}`);
-}, [data]);
+    }, [data]);
 
     return (
-    <div ref={svgRef} style={{width: 'auto', overflowX:'scroll'}}></div>)
+            <div ref={svgRef} style={{width: 'auto', overflowX:'scroll'}}></div>)
 };
 
 export { CandleStickChartObs };
