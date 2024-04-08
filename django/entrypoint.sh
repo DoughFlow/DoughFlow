157dc493ae47c1883c074f7d7dca4be01a631563
Marcus <marcusandrist53@gmail.com>
2024-04-08 11:10:21 -0500

    Finished container inits and compose file

--- django/entrypoint.sh
+++ django/entrypoint.sh
@@ -3,5 +3,8 @@
 # Apply Django database migrations
 python manage.py migrate
 
+# Add populate DB here
+python
+
 # Start Gunicorn server
 exec gunicorn django_server.wsgi:application --bind 0.0.0.0:8000
