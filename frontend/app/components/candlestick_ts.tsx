7304dd89a1f10fc67f2f42d9e826fdec5a011581
messerli-wallace <jjmw1058@gmail.com>
2024-04-21 16:09:37 -0500

    reorganization 1, more to come

--- frontend/app/components/candlestick_ts.tsx
+++ frontend/app/components/candlestick_ts.tsx
@@ -16,6 +16,178 @@ interface StockData {
     low: number;
     close: number;
 }
+
+/* 
+ * CandleChart from medium
+ * */
+const CandleChart = ({newData}: {newData: StockData[]}) => {
+  console.log("begining 'Medium' plot");
+  const data = newData;
+  //console.log(data.slice(0,2));
+  const svgRef = useRef(null);
+
+  useEffect(() => {
+
+    if(!data || data.length === 0) return;
+
+    d3.select(svgRef.current).selectAll('svg').remove();
+
+    const margin = { top: 20, right: 20, bottom: 30, left: 50 };
+    const width = (data.length * 75)
+    const new_width = (800);
+    //const width = svgRef.current.clientWidth - margin.left - margin.right;
+    const height = 500 - margin.top - margin.bottom;
+
+    // parent is parent
+    const parent = d3.select(svgRef.current)
+        .attr('width', new_width)
+        .attr('height', height + margin.top + margin.bottom)
+
+    // holds y axis
+    const y_axis_svg = parent
+        .append('svg')
+        .attr('height',height + margin.top + margin.bottom)
+        //.attr('width','')
+        .style("position", "absolute")
+        .style("pointer-events", "none")
+        .style("z-index", 1)
+
+    // body holds the plot with x axis
+    const body = parent.append('div')
+      .style('overflow-x','scroll')
+      .style("-webkit-overflow-scrolling", "touch");
+
+    // svg is the x axis and plot
+    const svg = body
+      .append('svg')
+      .attr('width', width)
+      .attr('height', height + margin.top + margin.bottom)    
+
+    const g = svg
+      .append('g')
+      .attr('transform', `translate(${margin.left},${margin.top})`);
+
+    const x = d3.scaleTime()
+              .range([0,width])
+
+    const y = d3.scaleLinear()
+              .range([height, 0])
+    
+    const xScale = d3.scaleBand()
+                      .range([0, width])
+                      .domain(data.map((d) => d.datetime)); // works even with errors
+
+    const yScale = d3.scaleLinear()
+                      .range([height, 0])
+                      .domain([d3.min(data, (d) => d.low), d3.max(data, (d) => d.high)]); // works with typescript errors
+
+    // Let's create a tooltip SVG text element
+    /* Source:
+     * https://observablehq.com/@john-guerra/how-to-add-a-tooltip-in-d3#cell-70
+     * */
+    const tooltip = g
+        .append("text")
+        .attr("class", "tooltip")
+        .attr("fill", "black")
+        .style("pointer-events", "none");
+    
+    g.selectAll('.candle')
+      .data(data)
+      .enter()
+      .append('rect')
+      .attr('class', 'candle')
+      .attr('x', d => xScale(d.datetime))
+      .attr('y', d => yScale(Math.max(d.open, d.close)))
+      .attr('width', "20px")
+      .attr('height', d => Math.abs(yScale(d.open) - yScale(d.close)))
+      .attr('fill', d => (d.close >= d.open ? 'green' : 'red'))
+      .on("mouseenter", (evt, d) => {
+              const [mx, my] = d3.pointer(evt);
+              const tooltipText =
+`Open: ${(d.open)}
+Close: ${(d.close)} 
+Low: ${(d.low)}
+High: ${(d.high)}`;
+              tooltip
+              .attr("transform", `translate(${mx}, ${my})`)
+              .selectAll("tspan")
+              .data(tooltipText.split("\n"))
+              .join("tspan")
+              .attr("dy", "1.5em")
+              .attr("x", "20px")
+              .text((text) => text );
+              })
+    .on("mouseout", () => tooltip.selectAll("tspan").remove());
+
+
+    g.selectAll(".wick")
+      .data(data)
+      .enter()
+      .append("rect")
+      .attr('class', 'wick')
+      .attr('x', d => (xScale(d.datetime) + 10))
+      .attr("y", d => yScale(d.high))
+      .attr('width', '1px')
+      .attr("height", d => (yScale(d.low) - yScale(d.high)))
+      .attr('fill', d => (d.close >= d.open ? 'green' : 'red'))
+
+
+
+    const xAxis = d3.axisBottom(xScale).tickSize(0).tickPadding(10);
+    const yAxis = d3.axisLeft(yScale).tickSize(0).tickPadding(10);
+
+
+    g.append('g').attr('transform', `translate(0,${height})`)
+      .call(xAxis
+        .ticks(d3.timeMonth.every(6))
+        .tickFormat(d3.timeFormat("%Y-%m-%d")))
+      .attr('dx', '10em')
+      .style('display', 'flex')
+      .style('text-anchor', 'center')
+      .style('font-size', '12px')
+      .style('margin', '20px')
+      .attr('overflow', 'scroll')
+      .style('color', 'black');
+    
+    y_axis_svg.append('g')
+        .call(yAxis)
+        .style('color', 'black')
+        .attr('text-anchor','start');
+
+    g.selectAll('xGrid')
+    .data(x.ticks().slice(1))
+    .join('line')
+    .attr('x1', d => x(d))
+    .attr('x2', d => x(d))
+    .attr('y1', 0)
+    .attr('y2', height)
+    .attr('stroke', "red")
+    .attr('stroke-width', .5);
+    
+    g.selectAll('yGrid')
+    .data(y.ticks().slice(1))
+    .join('line')
+    .attr('x1', 0)
+    .attr('x2', width)
+    .attr('y1', d => y(d))
+    .attr('y2', d => y(d))
+    .attr('stroke', "green")
+    .attr('stroke-width', .5);
+
+
+  }, [data]);
+
+
+  return (
+        <div ref={svgRef} ></div>
+  );
+};
+// old return 
+        {/*<div ref={svgRef} style={{width:'auto' , overflowX:'scroll'}} ></div>*/}
+export {CandleChart};
+
+
+
 /*
  * CandleStick from observables*/
 const CandleStickChartObs = ({data}: {data: StockData[]}) => {
@@ -118,153 +290,3 @@ High: ${formatValue(d.high)}`);
 };
 
 export { CandleStickChartObs };
-
-
-
-/* 
- * CandleChart from medium
- * */
-const CandleChart = ({newData}: {newData: StockData[]}) => {
-  console.log("begining 'Medium' plot");
-  const data = newData;
-  //console.log(data.slice(0,2));
-  const svgRef = useRef(null);
-
-  useEffect(() => {
-
-    if(!data || data.length === 0) return;
-
-    d3.select(svgRef.current).selectAll('svg').remove();
-
-    const margin = { top: 20, right: 20, bottom: 30, left: 50 };
-    const width = (data.length * 75)
-    //const width = svgRef.current.clientWidth - margin.left - margin.right;
-    const height = 500 - margin.top - margin.bottom;
-
-
-    const svg = d3.select(svgRef.current)
-      .append('svg')
-      .attr('width', width)
-      .attr('height', height + margin.top + margin.bottom);    
-
-    const g = svg
-      .append('g')
-      .attr('transform', `translate(${margin.left},${margin.top})`);
-
-    const x = d3.scaleTime()
-              .range([0,width])
-
-    const y = d3.scaleLinear()
-              .range([height, 0])
-    
-    const xScale = d3.scaleBand()
-                      .range([0, width])
-                      .domain(data.map((d) => d.datetime)); // works even with errors
-
-    const yScale = d3.scaleLinear()
-                      .range([height, 0])
-                      .domain([d3.min(data, (d) => d.low), d3.max(data, (d) => d.high)]); // works with typescript errors
-   
-    // Let's create a tooltip SVG text element
-    /* Source:
-     * https://observablehq.com/@john-guerra/how-to-add-a-tooltip-in-d3#cell-70
-     * */
-    const tooltip = g
-        .append("text")
-        .attr("class", "tooltip")
-        .attr("fill", "black")
-        .style("pointer-events", "none");
-    
-    g.selectAll('.candle')
-      .data(data)
-      .enter()
-      .append('rect')
-      .attr('class', 'candle')
-      .attr('x', d => xScale(d.datetime))
-      .attr('y', d => yScale(Math.max(d.open, d.close)))
-      .attr('width', "20px")
-      .attr('height', d => Math.abs(yScale(d.open) - yScale(d.close)))
-      .attr('fill', d => (d.close >= d.open ? 'green' : 'red'))
-      .on("mouseenter", (evt, d) => {
-              const [mx, my] = d3.pointer(evt);
-              const tooltipText =
-`Open: ${(d.open)}
-Close: ${(d.close)} (${(d.open, d.close)})
-Low: ${(d.low)}
-High: ${(d.high)}`;
-              tooltip
-              .attr("transform", `translate(${mx}, ${my})`)
-              .selectAll("tspan")
-              .data(tooltipText.split("\n"))
-              .join("tspan")
-              .attr("dy", "1.5em")
-              .attr("x", "20px")
-              .text((text) => text );
-              })
-    .on("mouseout", () => tooltip.selectAll("tspan").remove());
-
-
-    g.selectAll(".wick")
-      .data(data)
-      .enter()
-      .append("rect")
-      .attr('class', 'wick')
-      .attr('x', d => (xScale(d.datetime) + 10))
-      .attr("y", d => yScale(d.high))
-      .attr('width', '1px')
-      .attr("height", d => (yScale(d.low) - yScale(d.high)))
-      .attr('fill', d => (d.close >= d.open ? 'green' : 'red'))
-
-
-
-    const xAxis = d3.axisBottom(xScale).tickSize(0).tickPadding(10);
-    const yAxis = d3.axisLeft(yScale).tickSize(0).tickPadding(10);
-
-
-    g.append('g').attr('transform', `translate(0,${height})`)
-      .call(xAxis
-        .ticks(d3.timeMonth.every(6))
-        .tickFormat(d3.timeFormat("%Y-%m-%d")))
-      .attr('dx', '10em')
-      .style('display', 'flex')
-      .style('text-anchor', 'center')
-      .style('font-size', '12px')
-      .style('margin', '20px')
-      .attr('overflow', 'scroll')
-      .style('color', 'black');
-    
-    g.append('g')
-    .call(yAxis)
-    .style('color', 'black');
-
-    g.selectAll('xGrid')
-    .data(x.ticks().slice(1))
-    .join('line')
-    .attr('x1', d => x(d))
-    .attr('x2', d => x(d))
-    .attr('y1', 0)
-    .attr('y2', height)
-    .attr('stroke', "red")
-    .attr('stroke-width', .5);
-    
-    g.selectAll('yGrid')
-    .data(y.ticks().slice(1))
-    .join('line')
-    .attr('x1', 0)
-    .attr('x2', width)
-    .attr('y1', d => y(d))
-    .attr('y2', d => y(d))
-    .attr('stroke', "green")
-    .attr('stroke-width', .5);
-
-
-  }, [data]);
-
-
-  return (
-        <div ref={svgRef} style={{width:'auto' , overflowX:'scroll'}} ></div>
-  );
-};
-
-export {CandleChart};
-
