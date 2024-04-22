1c80fde8ce65885c7d80373f1c71b3d5f13e6872
Marcus <marcusandrist53@gmail.com>
2024-04-22 12:30:31 -0500

    Some todo done, lists gathered.

---
+++ scripts/api_to_data.py
@@ -0,0 +1,12 @@
+import os
+import requests
+import json
+
+def write_call(ticker):
+    response = requests.get(f"https://api.twelvedata.com/time_series?apikey=359b90199050434db9205eebad602506&interval=1day&format=JSON&symbol={ticker}&dp=2&start_date=2019-04-23 00:00:00&end_date=2024-04-22 00:00:00").json()
+    filename = f"{ticker}.json"
+    path = os.path.join("data/",filename)
+    with open(path, 'w') as file:
+        json.dump(response, file, indent=4)
+
+    print(f"Response written to {filename}")
