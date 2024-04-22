878760dbfd3488bab7469620e6a668dfe98b09f0
Marcus Andrist <marcusandrist53@gmail.com>
2024-04-22 16:38:17 -0500

    Modified config

--- next/tailwind.config.ts
+++ next/tailwind.config.ts
@@ -3,6 +3,7 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/*",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
