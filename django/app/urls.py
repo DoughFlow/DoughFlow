0b99bf85df74ae252afc0a17489792f2e9d6347a
Marcus <marcusandrist53@gmail.com>
2024-04-28 21:29:53 -0500

    165203-fix commits to be squashed here165203$

--- django/app/urls.py
+++ django/app/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     #path('data/', StockDataView.as_view()),
     path('filter/<str:ticker>/start=<str:start_date>_end=<str:end_date>/', StockFilterView.as_view(), name='stock-data'),
     path('preview/<str:ticker>/', StockPreviewView.as_view(), name='stock-preview'),
-    path('<str:ticker>/6m', StockOneYearView.as_view(), name='one-year-data'),
+    path('<str:ticker>/6m', StockSixMonthView.as_view(), name='one-year-data'),
     path('<str:ticker>/1y', StockOneYearView.as_view(), name='one-year-data'),
-    path('<str:ticker>/3y', StockOneYearView.as_view(), name='one-year-data'),
-    path('<str:ticker>/5y', StockOneYearView.as_view(), name='one-year-data'),
+    path('<str:ticker>/3y', StockThreeYearView.as_view(), name='one-year-data'),
+    path('<str:ticker>/5y', StockFiveYearView.as_view(), name='one-year-data'),
 ]
