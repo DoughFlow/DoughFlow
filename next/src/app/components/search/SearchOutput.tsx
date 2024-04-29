ba51a7b99574d0a411923edab2207a75f7684274
EthanRohman93 <ethanmrohman@gmail.com>
2024-04-29 02:38:50 -0500

    routing and search link fixed

--- next/src/app/components/search/SearchOutput.tsx
+++ next/src/app/components/search/SearchOutput.tsx
@@ -9,7 +9,7 @@ interface Props {
 
 const SearchOutput = ({ result }: Props) => {
     return (
-        <Link href={`/base/${result}`} className="block px-1 py-2 my-4 w-full text-left hover:bg-gray-100 focus:bg-gray-100">
+        <Link href={`/${result}`} className="block px-1 py-2 my-4 w-full text-left hover:bg-gray-100 focus:bg-gray-100">
             <div className='flex justify-between items-center w-full'>
                 <div className='flex-1 text-2xl'>
                     {result}
