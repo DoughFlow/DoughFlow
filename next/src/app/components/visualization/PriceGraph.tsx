7d652af91e42a3f931893bbd8a4717d18c177f22
messerli-wallace <jjmw1058@gmail.com>
2024-05-07 10:53:47 -0500

    Cleaned up PriceGraph

--- next/src/app/components/visualization/PriceGraph.tsx
+++ next/src/app/components/visualization/PriceGraph.tsx
@@ -4,7 +4,7 @@ import * as d3 from "d3";
 
 /**
  * Candlestick Graph component for visualizing stock price data.
- * 
+ *
  * @param ticker - Ticker symbol for the stock data.
  * @returns Candlestick Graph component.
  */
@@ -27,17 +27,36 @@ const PriceGraph = ({ ticker }: { ticker: string }) => {
     d3.select(svgRef.current).selectAll("svg").remove();
 
     const margin = { top: 20, right: 20, bottom: 30, left: 50 };
-    const total_width = data.length * 16;
+    const total_width = data.length * 12;
     const component_width = 700;
     //const width = svgRef.current.clientWidth - margin.left - margin.right;
     const height = 350 - margin.top - margin.bottom;
 
-    // parent is parent
+    // Helper function for tooltip position
+    const tooltipHelper = (x: number, y: number): [number, number] => {
+      // Adjust tooltip position if it exceeds screen boundaries
+      let adjustedX = x + 10;
+      let adjustedY = y + 10;
+
+      if (adjustedY + 150 > height) {
+        adjustedY = height - 150;
+      }
+      switch (true) {
+        case adjustedX + 300 > window.innerWidth:
+          adjustedX = window.innerWidth - 300;
+          break;
+        case adjustedX + 250 > total_width:
+          adjustedX = total_width - 250;
+          break;
+      }
+      return [adjustedX, adjustedY];
+    };
+
+    // Begin defining plot stuff
     const parent = d3
       .select(svgRef.current)
       .attr("width", component_width)
-      .attr("height", height + margin.top + margin.bottom)
-      .attr("overflow-x", "hidden");
+      .attr("height", height + margin.top + margin.bottom);
 
     // holds y axis
     const y_axis_svg = parent
@@ -49,7 +68,7 @@ const PriceGraph = ({ ticker }: { ticker: string }) => {
       .style("z-index", 1);
 
     // body holds the plot with x axis
-    const body = parent.append("div").style("overflow-x", "scroll");
+    const body = parent.append("div").style("overflow-x", "auto");
 
     // svg is the x axis and plot
     const svg = body
@@ -61,7 +80,8 @@ const PriceGraph = ({ ticker }: { ticker: string }) => {
       .append("g")
       .attr("transform", `translate(${margin.left},${margin.top})`);
 
-    const x = d3.scaleTime().range([0, total_width]);
+    //const x = d3.scaleBand().range([0, total_width])
+    //.domain(data.map((d) => d.timestamp));
 
     const y = d3.scaleLinear().range([height, 0]);
 
@@ -83,19 +103,18 @@ const PriceGraph = ({ ticker }: { ticker: string }) => {
       .attr("fill", "white")
       .style("pointer-events", "none");
 
-    g.selectAll(".candle")
+    // Vertical bars spanning entire graph for tooltip
+    g.selectAll(".xGroup")
       .data(data)
       .enter()
       .append("rect")
-      .attr("class", "candle")
+      .attr("class", "xBar")
       .attr("x", (d) => xScale(d.timestamp)!)
-      .attr("y", (d) => yScale(Math.max(d.open_price, d.close_price)))
-      .attr("width", "16px")
-      .attr("height", (d) =>
-        Math.abs(yScale(d.open_price) - yScale(d.close_price)),
-      )
-      .attr("fill", (d) => (d.close_price >= d.open_price ? "green" : "red"))
-      .on("mouseenter", (evt, d) => {
+      .attr("y", 0)
+      .attr("width", "12px")
+      .attr("height", height)
+      .attr("fill", "dfgrey")
+      .on("mouseover", (evt, d) => {
         const [mx, my] = d3.pointer(evt);
         let tooltipText = `Date: ${d.timestamp}
 Open: ${d.open_price}
@@ -106,8 +125,54 @@ High: ${d.high_price}`;
         if (d.volume !== undefined) {
           tooltipText += `\nVolume: ${d.volume}`;
         }
+
+        const tooltipPosition = tooltipHelper(mx, my);
+
         tooltip
-          .attr("transform", `translate(${mx}, ${my})`)
+          .attr(
+            "transform",
+            `translate(${tooltipPosition[0]}, ${tooltipPosition[1]})`,
+          )
+          .selectAll("tspan")
+          .data(tooltipText.split("\n"))
+          .join("tspan")
+          .attr("dy", "1.5em") // spacing between the lines
+          .attr("x", "20px") // Space to the right of the cursor
+          .text((text) => text);
+      })
+      .on("mouseout", () => tooltip.selectAll("tspan").remove());
+
+    g.selectAll(".candle")
+      .data(data)
+      .enter()
+      .append("rect")
+      .attr("class", "candle")
+      .attr("x", (d) => xScale(d.timestamp)!)
+      .attr("y", (d) => yScale(Math.max(d.open_price, d.close_price)))
+      .attr("width", "12px")
+      .attr("height", (d) =>
+        Math.abs(yScale(d.open_price) - yScale(d.close_price)),
+      )
+      .attr("fill", (d) => (d.close_price >= d.open_price ? "green" : "red"))
+      .on("mouseover", (evt, d) => {
+        const [mx, my] = d3.pointer(evt);
+        let tooltipText = `Date: ${d.timestamp}
+Open: ${d.open_price}
+Close: ${d.close_price} 
+Low: ${d.low_price}
+High: ${d.high_price}`;
+
+        if (d.volume !== undefined) {
+          tooltipText += `\nVolume: ${d.volume}`;
+        }
+
+        const tooltipPosition = tooltipHelper(mx, my);
+
+        tooltip
+          .attr(
+            "transform",
+            `translate(${tooltipPosition[0]}, ${tooltipPosition[1]})`,
+          )
           .selectAll("tspan")
           .data(tooltipText.split("\n"))
           .join("tspan")
@@ -122,7 +187,7 @@ High: ${d.high_price}`;
       .enter()
       .append("rect")
       .attr("class", "wick")
-      .attr("x", (d) => xScale(d.timestamp)! + 8)
+      .attr("x", (d) => xScale(d.timestamp)! + 6)
       .attr("y", (d) => yScale(d.high_price))
       .attr("width", "1px")
       .attr("height", (d) => yScale(d.low_price) - yScale(d.high_price))
@@ -153,14 +218,18 @@ High: ${d.high_price}`;
       .attr("text-anchor", "end");
 
     g.selectAll("xGrid")
-      .data(x.ticks().slice(1))
-      .join("line")
-      .attr("x1", (d) => x(d))
-      .attr("x2", (d) => x(d))
-      .attr("y1", 0)
-      .attr("y2", height)
-      .attr("stroke", "red")
-      .attr("stroke-width", 0.5);
+      .data(data.filter((d, i) => isMonday(d) && i % 4 === 0))
+      .enter()
+      .append("line")
+      .attr("class", "vertical-line")
+      .attr("x1", (d) => xScale(d.timestamp)!) // Center of the bar
+      .attr("y1", height)
+      .attr("x2", (d) => xScale(d.timestamp)!) // Center of the bar
+      .attr("y2", 0)
+      //.attr("stroke", "white")
+      .classed("stroke-dfbrown", true)
+      .attr("stroke-width", 1);
+
     g.selectAll("yGrid")
       .data(y.ticks().slice(1))
       .join("line")
@@ -173,27 +242,33 @@ High: ${d.high_price}`;
 
     // tooltip needs to be on top of the other graph elements
     tooltip.raise();
-
-    // Delete the extra useless scrollbar we get for some reason
-    parent.selectAll<HTMLDivElement, unknown>("div").each(function () {
-      if (this.childNodes.length === 0 || !this.textContent?.trim()) {
-        d3.select(this).remove();
-      }
-    });
   }, [data]);
 
-  // Helper function to check date for x axis
+  // Helper function for mondayFilter and for x-axis bars
+  const isMonday = (d: DataPoint): boolean => {
+    const date = new Date(d.timestamp);
+    if (date.getDay() === 1) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+  // Helper function to get every other monday for x axis
   const mondayFilter = (data: DataPoint[]): string[] => {
     const mondays: string[] = [];
+    const everyOtherMonday: string[] = [];
 
     data.forEach((obj) => {
-      const date = new Date(obj.timestamp);
-      if (date.getDay() === 1) {
+      if (isMonday(obj)) {
         mondays.push(obj.timestamp);
       }
     });
 
-    return mondays;
+    for (let i = 0; i < mondays.length; i += 2) {
+      everyOtherMonday.push(mondays[i]);
+    }
+
+    return everyOtherMonday;
   };
 
   return <div ref={svgRef}></div>;
