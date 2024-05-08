cd65946edcfbef6280a9724cb8d45b492d62f2c0
EthanRohman93 <ethanmrohman@gmail.com>
2024-05-08 14:06:05 -0500

    added model

--- django/app/models.py
+++ django/app/models.py
@@ -16,3 +16,19 @@ class StockMarketData(models.Model):
     class Meta:
         db_table = 'stock_data'
         unique_together = (('timestamp', 'ticker'),)
+
+
+class StockIndicatorData(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    timestamp = models.DateField()
+    ticker = models.CharField(max_length=8)
+    open_price = models.DecimalField(max_digits=10, decimal_places=2)
+    high_price = models.DecimalField(max_digits=10, decimal_places=2)
+    low_price = models.DecimalField(max_digits=10, decimal_places=2)
+    close_price = models.DecimalField(max_digits=10, decimal_places=2)
+    volume = models.IntegerField()
+    candle_time = models.IntegerField(null=True, blank=True)
+
+    class Meta:
+        db_table = 'stock_data'
+        unique_together = (('timestamp', 'ticker'),)
---
+++ etl/extract/utils/run.py
@@ -0,0 +1,8 @@
+"""
+TODO
+allow for user to enter a list of [keys], [ticker], [price|indicator]
+this will then return a transformed data set with this form
+
+
+
+"""
