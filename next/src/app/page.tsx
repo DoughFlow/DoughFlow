a4c049b77d6f8b3fc44d78847c10b90f487161f0
EthanRohman93 <ethanmrohman@gmail.com>
2024-05-10 09:05:24 -0500

    everything is working

--- next/src/app/page.tsx
+++ next/src/app/page.tsx
@@ -2,7 +2,7 @@ import Search from "./components/search/Search";
 
 export default function Home() {
     return(
-        <div className="flex flex-col justify-center items-center">
+        <div className="flex flex-col justify-center items-centersm:flex sm:flex-col sm:items-center">
             <Search />
         </div>
 );
