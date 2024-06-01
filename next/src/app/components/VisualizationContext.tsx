1e6bae66bb3a359bbf4750f1afcfcf4fcc464ea3
EthanRohman93 <ethanmrohman@gmail.com>
2024-06-01 13:27:10 -0500

    visualization context and context.json'

---
+++ next/src/app/components/VisualizationContext.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+type Props = {
+    inp: string;
+}
+
+const VisualizationContext = ({ inp }: Props) => {
+
+    return (
+      <div className='border-2 text-5xl'>
+        beast component
+      </div>
+    );
+};
+
+export default VisualizationContext;
