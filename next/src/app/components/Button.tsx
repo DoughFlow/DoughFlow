c00961238d06e655fffa19d061231d9a8fa510fc
Marcus <marcusandrist53@gmail.com>
2024-06-06 16:33:07 -0500

    Yup

--- next/src/app/components/Button.tsx
+++ next/src/app/components/Button.tsx
@@ -1,24 +1,47 @@
 'use client'
 import React, { useState } from 'react';
+import { Rnd } from 'react-rnd';
+
+const resize_config = { top:false, right:false, bottom:false,
+            left:false, topRight:false, bottomRight:false, 
+            bottomLeft:false, topLeft:false };
+
 
 const Button: React.FC<
-{ onClick: (event: any) => void, onDrag: (event: any) => void,
-area: number}> = ({onClick, onDrag, area}) => {
-  const styles:any=[{position:"absolute",left:"100px", top:"100px"},
-                {position:"absolute",right:"100px", top:"100px"},
-                {position:"absolute",right:"100px", bottom:"100px"},
-                {position:"absolute",left:"100px", bottom:"100px"}]
+{ onClick: (event: any) => void, posn_x: number, posn_y: number}> = 
+({onClick, posn_x, posn_y}) => {
 
 //const [area,Setarea] = useState(0);
-  
-  return (<button
-      onClick={onClick}
-      draggable={true}
-      onDragEnter={onDrag}
-      style={styles[area]}
-      >
-    Drag or Click
-    </button>);
+ /** 
+  return (
+    <Rnd>
+      <div className="bg-white">
+        <button onClick={onClick}>
+          Drag or Click
+        </button>
+      </div>
+    </Rnd>);
+**/
+return (<Rnd
+  dragHandleClassName="handle"
+  cancel=".cancel-drag"
+  default={{
+    x: 55, y: 55, width: 115, height: 65
+  }}
+  enableResizing={resize_config}
+  >
+  <div>
+    <div className="handle">Drag from here</div>
+    <div className="cancel-drag">
+      <button className="text-center" onClick={onClick}>
+        button
+      </button>
+    </div>
+  </div>
+</Rnd>);
+
+
+
 };
 
 export default Button;
