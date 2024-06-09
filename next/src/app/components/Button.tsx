bdfd93dbd06043135d441c39ad3dfdfd54637773
Marcus <marcusandrist53@gmail.com>
2024-06-09 16:08:13 -0500

    Graph with button and context endpoint added

--- next/src/app/components/Button.tsx
+++ next/src/app/components/Button.tsx
@@ -1,47 +1,28 @@
-'use client'
-import React, { useState } from 'react';
+import React from 'react';
 import { Rnd } from 'react-rnd';
+
+const Button: React.FC<{ onClick: (event: any) => void}> = ({onClick}) => {
+
+  return (<Rnd dragHandleClassName="handle" cancel=".cancel-drag"
+            default = {{x: 55, y: 55, width: 115, height: 65}}
+            enableResizing={resize_config} >
+
+            <p className="handle bg-gray-500 text-right outline-dotted">
+              Drag Area
+            </p>
+            <div className="cancel-drag text-center bg-green-700">
+              Drug Content
+              <button onClick={onClick}>
+                Durg Burton
+              </button>
+            </div>    
+          </Rnd>
+        );
+
+};
 
 const resize_config = { top:false, right:false, bottom:false,
             left:false, topRight:false, bottomRight:false, 
             bottomLeft:false, topLeft:false };
-
-
-const Button: React.FC<
-{ onClick: (event: any) => void, posn_x: number, posn_y: number}> = 
-({onClick, posn_x, posn_y}) => {
-
-//const [area,Setarea] = useState(0);
- /** 
-  return (
-    <Rnd>
-      <div className="bg-white">
-        <button onClick={onClick}>
-          Drag or Click
-        </button>
-      </div>
-    </Rnd>);
-**/
-return (<Rnd
-  dragHandleClassName="handle"
-  cancel=".cancel-drag"
-  default={{
-    x: 55, y: 55, width: 115, height: 65
-  }}
-  enableResizing={resize_config}
-  >
-  <div>
-    <div className="handle">Drag from here</div>
-    <div className="cancel-drag">
-      <button className="text-center" onClick={onClick}>
-        button
-      </button>
-    </div>
-  </div>
-</Rnd>);
-
-
-
-};
 
 export default Button;
---
+++ next/src/app/components/DoughImports.tsx
@@ -0,0 +1,7 @@
+export { default as Search } from "./Search";
+export { default as VisualizationContext } from "./VisualizationContext";
+export { default as Controller } from "./Controller";
+export { default as Button } from "./Button";
+export { default as List } from "./List";
+export type { Stock } from "./GlobalContextProvider";
+export { useGlobal } from "./GlobalContextProvider";
