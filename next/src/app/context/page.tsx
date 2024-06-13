2a9bf3c7776cec8e90213182df62a4c20f2d0734
EthanRohman93 <ethanmrohman@gmail.com>
2024-06-12 19:20:56 -0500

    working context

--- next/src/app/context/page.tsx
+++ next/src/app/context/page.tsx
@@ -1,39 +1,42 @@
 "use client"
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useStocks } from '@/components/StockContext';
 
 const Context = () => {
-    const { stocks, initStock, updateStock, removeStock, renderPrevious } = useStocks();
+    const { stocks, initStock, updateStock, renderPrevious, removeAndRender } = useStocks();
     useEffect(() => {
-          initStock("aapl");
+      initStock("aapl");
     }, []);
 
     const addStock = (index: number) => {
-        const newStock = {ticker: "amzn", value: "price", time: "1y"};
-        updateStock(index, newStock);
-        renderPrevious();
+      const newStock = {ticker: "amzn", value: "price", time: "1y"};
+      updateStock(index, newStock);
+      renderPrevious();
     };
 
-    return (
-        <div className=''>
-            <div>{stocks.length}</div>
-            {stocks.map((stock, index) => (
-              <div key={index}>
-                <div>Ticker: {stock.ticker}</div>
-                <div>Value: {stock.value}</div>
-                <div>Time: {stock.time}</div>
-                <div className=''>Svg: {stock.svg}</div>
-                <div onClick={() => removeStock(index)}>remove</div>
-              </div>
-            ))}
+  const dropStock = async (index: number) => {
+    removeAndRender(index);
+  };
 
-            <div onClick={() => addStock(0)}>add stock in index 0</div>
-            <div onClick={() => addStock(1)}>add stock in index 1</div>
-            <div onClick={() => addStock(2)}>add stock in index 2</div>
-            <div onClick={() => addStock(3)}>add stock in index 3</div>
-            <div onClick={() => addStock(4)}>add stock in index 4</div>
+  return (
+    <div className=''>
+      <div>{stocks.length}</div>
+      {stocks.map((stock, index) => (
+        <div key={index}>
+          <div>Ticker: {stock.ticker}</div>
+          <div>Value: {stock.value}</div>
+          <div>Time: {stock.time}</div>
+          <div className=''>Svg: {stock.svg}</div>
+          <div onClick={() => dropStock(index)}>remove</div>
         </div>
-    );
+      ))}
+
+      <div onClick={() => addStock(1)}>add stock in index 1</div>
+      <div onClick={() => addStock(2)}>add stock in index 2</div>
+      <div onClick={() => addStock(3)}>add stock in index 3</div>
+      <div onClick={() => addStock(4)}>add stock in index 4</div>
+    </div>
+  );
 };
 
 export default Context;
