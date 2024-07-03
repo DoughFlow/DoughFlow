d6f0a206bf160348da64230f210cb9173c95da58
Marcus <marcusandrist53@gmail.com>
2024-07-03 15:02:57 -0500

    In-progress info page

--- next/src/app/info/page.tsx
+++ next/src/app/info/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 const Info = () => {
   return (  
-  <div className="bg-dfBlack md:bg-dfWhite">
+  <div className="bg-dfBlack">
 
   <div className="drop-shadow-2xl min-w-screen bg-dfBrown text-[2.25rem]
   text-dfWhite py-[.75rem] flex flex-row justify-center items-center border-b-[.55rem] 
@@ -32,47 +32,48 @@ const Info = () => {
     insightful visualizations of stock data using an intuitive UI.
   </h2>
   <ul className="pt-[.75rem] text-[.8rem] md:desktop-technologies-list">
-    <li className="flex flex-row items-center mb-[2.25rem] md:mb-[5rem]">
+    <li className="flex flex-row items-center mb-[2.25rem] md:mb-[2.5rem] border-2">
       <Image src="/next.png" alt="NextJS" width={50} height={50} className="md:hidden min-w-[3rem] mr-3"/>
       <Image src="/next.png" alt="NextJS" width={100} height={100} className="hidden md:block min-w-[4rem] mr-5"/>
-      Well next was really good and we utilized it well, it was difficult because
-      of newer docs and no ChatGPT and yeawh overall it was a really great time
-      and we struggled with ex wqawhyt and z buit it went good.
+      Using NextJS allowed us to learn TypeScript and be exposed to a front-end
+      react framework. Because of its recency, relying solely on the docs to
+      develop proved to be a satisfying challenge and limited our use of
+      LLMs.
     </li>
-    <li className="flex flex-row items-center mb-[2.25rem] md:mb-[5rem]">
+    <li className="flex flex-row items-center mb-[2.25rem] md:mb-[2.5rem] border-2">
       <Image src="/django.png" alt="Django" width={50} height={50} className="md:hidden min-w-[3rem] mr-3"/>
       <Image src="/django.png" alt="Django" width={50} height={50} className="hidden md:block min-w-[5rem] mr-5"/>
       Django Rest Framework was used to create our back-end API, utilizing its
       Object Relational Model, we were able to write all of our Database
       models and interactions in Python rather than SQL.
     </li>
-    <li className="flex flex-row items-center mb-[2.25rem] md:mb-[5rem]">
+    <li className="flex flex-row items-center mb-[2.25rem] md:mb-[2.5rem] border-2">
       <Image src="/postgres.png" alt="PostgreSQL" width={50} height={50} className="md:hidden min-w-[3rem] mr-3"/>
       <Image src="/postgres.png" alt="PostgreSQL" width={50} height={50} className="hidden md:block min-w-[5rem] mr-5"/>
       PostgreSQL served as a robust database engine and object-relational
       database management system. Its extensive documentation and open-source
-      status allowed us to get off the ground and dockerize our DB environment
-      quickly and effectively.
+      status allowed us to get off the ground quickly. Our production database
+      has upwards of 5 million rows.
     </li>
-    <li className="flex flex-row items-center mb-[2.25rem] md:mb-[5rem]">
+    <li className="flex flex-row items-center mb-[2.25rem] md:mb-[2.5rem] border-2">
       <Image src="/aws.png" alt="AWS" width={50} height={50} className="md:hidden min-w-[3rem] mr-3"/>
       <Image src="/aws.png" alt="AWS" width={100} height={100} className="hidden md:block min-w-[5rem] mr-5"/>
       Amazon Web Services was critical to the delivery of, and the hosting for
-      application. Our most used services included Amazon EC2, IAM Identity Center, 
+      our application. Our most used services included Amazon EC2, IAM Identity Center, 
       and Amazon Secrets Manager.
     </li>
-    <li className="flex flex-row items-center mb-[2.25rem] md:mb-[5rem]">
+    <li className="flex flex-row items-center mb-[2.25rem] md:mb-[2.5rem] border-2">
       <Image src="/docker.png" alt="NextJS" width={50} height={50} className="md:hidden min-w-[3rem] mr-3"/>
       <Image src="/docker.png" alt="NextJS" width={50} height={50} className="hidden md:block min-w-[5rem] mr-5"/>
       Our application is succesfully dockerized, scalable, and deployable via
       our custom GitHub-integrated deployment pipeline written in bash. We also
       leveraged Docker to create consistent development environments.
     </li>
-    <li className="flex flex-row items-center mb-[2.25rem] md:mb-[5rem]">
+    <li className="flex flex-row items-center mb-[2.25rem] md:mb-[2.5rem] border-2">
       <Image src="/nginx.png" alt="nginx" width={50} height={50} className="md:hidden min-w-[3rem] mr-3"/>
       <Image src="/nginx.png" alt="nginx" width={50} height={50} className="hidden md:block min-w-[5rem] mr-5"/>
       Nginx is the core web server that allows our app to reverse proxy client-side
-      requests, enabling more robust security, deployment, and load-balancing.
+      requests. It enabled more robust security and easier deployment.
     </li>
   </ul>
   <h1 className="text-[2rem] md:text-[5rem] w-max px-2 rounded-sm">
@@ -83,7 +84,7 @@ const Info = () => {
   </h1>
   <div className="flex flex-col">
     <div className="flex flex-row justify-between mb-[1.5rem] md:justify-center">
-      <div className="w-[9rem] bg-dfYellow bg-opacity-75 rounded-sm
+      <div className="w-[11rem] bg-dfYellow bg-opacity-75 rounded-sm
       md:desktop-hero">
         <a href="https://ethanrohman.com/">
           <h1 className="flex justify-center md:text-[3rem]">
@@ -112,7 +113,7 @@ const Info = () => {
           </li>
         </ul>
       </div>
-      <div className="w-[9rem] bg-dfYellow bg-opacity-75 rounded-sm
+      <div className="w-[11rem] bg-dfYellow bg-opacity-75 rounded-sm
       md:desktop-hero">
         <a href="">
           <h1 className="flex justify-center items-center md:text-[3rem]">
@@ -141,7 +142,7 @@ const Info = () => {
     </div>
     <div className="flex flex-row justify-between md:justify-center mb-[4rem] 
     md:mb-[10rem]">
-      <div className="w-[9rem] bg-dfYellow bg-opacity-75 rounded-sm
+      <div className="w-[11rem] bg-dfYellow bg-opacity-75 rounded-sm
       md:desktop-hero">
         <a href="https://marcusandrist.github.io/">
           <h1 className="flex justify-center md:text-[3rem]">
@@ -149,7 +150,7 @@ const Info = () => {
           </h1>
         </a>
           <h2 className="bg-dfGray bg-opacity-75 mx-[.25rem] text-[.75rem] px-1 
-          flex justify-cente md:text-[1.25rem]">
+          flex justify-center md:text-[1.25rem]">
             Full-Stack Developer + Scrum Master
           </h2>
         <ul className="ml-[.33rem] text-[.85rem] md:desktop-notable-work">
@@ -167,7 +168,7 @@ const Info = () => {
           </li>
         </ul>
       </div>
-      <div className="w-[9rem] bg-dfYellow bg-opacity-75 rounded-sm
+      <div className="w-[11rem] bg-dfYellow bg-opacity-75 rounded-sm
       md:desktop-hero">
         <a href="">
           <h1 className="flex justify-center items-center md:text-[3rem]">
@@ -186,7 +187,7 @@ const Info = () => {
             -one thang
           </li>
           <li>
-            -one thang
+            -one than
           </li>
         </ul>
       </div>
